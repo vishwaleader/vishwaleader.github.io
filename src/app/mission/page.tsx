db7783cdc0216d48c47de863e5a9700d6c9ebbdf
vishwaleader <vishwaleader@users.noreply.github.com>
2026-07-08 21:52:31 +0530

    feat: add scroll to top on mission page and tint DACCI logo dark navy blue

--- src/app/mission/page.tsx
+++ src/app/mission/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import GlobalPreloader from "@/components/GlobalPreloader";
 
 export default function MissionPage() {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <GlobalPreloader />
